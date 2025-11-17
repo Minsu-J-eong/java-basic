666195e9bce1308505662e64b1003b708249c85c
Minsu-J-eong <soom990906@gmail.com>
2025-11-17 21:42:30 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// HelloWorld
+		System.out.println("Hello World!\n");
+		System.out.println("2025-11-17");
+		System.out.println("정민수");
+	}
+
+}
